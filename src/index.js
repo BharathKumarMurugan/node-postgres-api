7c24bb27a5948a513317d47eaf4c93af66fb9c7c
Bharath Kumar Murugan <bharathkumarmurugan2016@gmail.com>
2024-06-23 18:42:41 +0530

    added CORS and error handling

--- src/index.js
+++ src/index.js
@@ -1,25 +1,35 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const db = require('./queries');
+const cors = require("cors");
+const db = require("./queries");
 
 const app = express();
 const PORT = process.env.NODE_API_PORT || 3000;
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
     extended: true,
   })
 );
+app.use((req, res, next) => {
+  const error = new Error("Something went wrong");
+  next(error);
+});
+app.use((err, req, res, next) => {
+  console.error("Error: ", err.message);
+  res.status(500).send("Internal Server error");
+});
 
 app.get("/", (req, res) => {
   res.send("All set to go");
 });
 
-app.get('/users', db.getUsers)
-app.get('/users/:id', db.getUserById)
-app.post('/users', db.createUser)
-app.put('/users/:id', db.updateUser)
-app.delete('/users/:id', db.deleteUser)
+app.get("/users", db.getUsers);
+app.get("/users/:id", db.getUserById);
+app.post("/users", db.createUser);
+app.put("/users/:id", db.updateUser);
+app.delete("/users/:id", db.deleteUser);
 
 app.listen(PORT, () => console.log(`Server is running at port ${PORT}`));
